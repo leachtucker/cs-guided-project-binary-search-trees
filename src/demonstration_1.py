a65f2668f0f66bdfc5a6a574c89337a3c69acaaf
leachtucker <15221354+leachtucker@users.noreply.github.com>
2021-03-09 20:15:17 -0800

    Created a basic solution to demo1

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -23,7 +23,29 @@ class BinaryTreeNode:
         self.value = value
         self.left = left
         self.right = right
-
-def maxDepth(self, root):
+    
+def maxDepth(root):
     # Your code here
+    print(root.value)
 
+    if root is None:
+        return 0
+
+    if root.left is None and root.right is None:
+        return 1
+    else:
+        left_depth = maxDepth(root.left)
+        right_depth = maxDepth(root.right)
+
+        if left_depth > right_depth:
+            return left_depth + 1
+        else:
+            return right_depth + 1
+
+root = BinaryTreeNode(5)
+root.right = BinaryTreeNode(32)
+root.right.right = BinaryTreeNode(4)
+root.right.left = BinaryTreeNode(8)
+root.left = BinaryTreeNode(12)
+
+print(maxDepth(root))
